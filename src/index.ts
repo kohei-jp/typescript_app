8449762eb1668a9d63a9b14c63ad1dd2e390206b
kohei-jp <norwaymi38@gmail.com>
2022-05-24 21:40:55 +0900

    DOM操作

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,36 @@
-let nickname:HTMLInputElement
-let message:HTMLInputElement
-let table:HTMLTableElement
-const url = 'https://typescript-app-e6875-default-rtdb.firebaseio.com/boards.json'
-
-function doAction():void {
-    const data = {
-        nickname: nickname.value,
-        message: message.value,
-        posted: new Date().getTime()
-    }
-    sendData(url, data)
-}
-
-function doDelete():void {
-    fetch(url, {
-        method:'DELETE'
-    }).then(res=>{
-        console.log(res.statusText)
-        getData(url)
-    })
-}
-
-function sendData(url:string, data:object) {
-    fetch(url, {
-        method:'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(res=>{
-        console.log(res.statusText)
-        getData(url)
-    })
-}
-
-function getData(url:string) {
-    fetch(url).then(res=>res.json()).then(re=> {
-        let result = `<thead>
-                          <tr><th>Message</th>
-                          <th>Nickname</th>
-                          <th>posted</th></tr>
-                      </thead><tbody>`
-        let tb = ''
-        for(let ky in re) {
-            let item = re[ky]
-            tb = '<tr><td>' + item['message'] + '</td><td>'
-                + item['nickname'] + '</td><td>' 
-                + new Date(item['posted']).toLocaleString()
-                + '</td></tr>' + tb
-        }
-        result += tb + '</tbody>'
-        table.innerHTML = result
-    })
-}
+let msg:HTMLParagraphElement
+const html = `<h3>This is message</h3>
+  <div id="content">wait...</div>`
 
 window.addEventListener('load',()=>{
-    message = document.querySelector('#message')
-    nickname = document.querySelector('#nickname')
-    table = document.querySelector('#table')
-    const btn :HTMLButtonElement =  document.querySelector('#btn')
-    btn.onclick = doAction
-    const del :HTMLButtonElement =  document.querySelector('#delete')
-    del.onclick = doDelete
-    getData(url)
+  msg = document.querySelector('#msg')
+  msg.innerHTML = html
+  const content:HTMLDivElement = document.querySelector('#content')
+  setDiv(content)
 })
+
+const addElement = function(event:MouseEvent) {
+  const div:HTMLDivElement = document.createElement('div')
+  div.style.position = "absolute"
+  div.style.width = "50px"
+  div.style.height = "50px"
+  div.style.left = event.offsetX - 25 + "px"
+  div.style.top = event.offsetY - 25 + "px"
+  div.style.backgroundColor = "#cc00aa33"
+  const target:HTMLElement = event.target as HTMLElement
+  target.appendChild(div)
+}
+
+function setDiv(content:HTMLDivElement) {
+  content.style.position = "absolute"
+  content.style.left = "100px"
+  content.style.top = "100px"
+  content.style.width ="300px"
+  content.style.height = "300px"
+  content.style.borderWidth = "3px"
+  content.style.borderStyle = "solid"
+  content.style.borderColor = "red"
+  content.style.backgroundColor = "white"
+  content.textContent = ""
+  content.addEventListener('click', addElement)
+}
